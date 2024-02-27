6dd52d30bf0fed3f070282f9105f483e04833579
Dima <dima@email.de>
2024-02-27 19:36:23 +0100

    nav bar ergänzt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { CustomersComponent } from './customers/customers.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    NavigationBarComponent,
+    CustomersComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'shopDemoClient';
 }
